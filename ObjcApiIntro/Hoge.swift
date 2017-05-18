1a125baf2574eb50117191f0958974b1762a9cd1
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2017-05-18 16:13:14 +0900

    add swift code

---
+++ ObjcApiIntro/Hoge.swift
@@ -0,0 +1,14 @@
+//
+//  Hoge.swift
+//  ObjcApiIntro
+//
+//  Copyright © 2017年 hormiga6. All rights reserved.
+//
+
+import Foundation
+
+public class Hoge {
+  func getName() -> String {
+    return "Hoge"
+  }
+}
