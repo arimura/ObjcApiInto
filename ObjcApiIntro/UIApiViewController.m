e618a19d91f71e897613b2ad99f7bb8984d6b936
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2017-07-18 22:50:57 +0900

    moving view sample

--- ObjcApiIntro/UIApiViewController.m
+++ ObjcApiIntro/UIApiViewController.m
@@ -6,26 +6,47 @@
 //
 
 #import "UIApiViewController.h"
+#import <stdlib.h>
 
 @interface UIApiViewController ()
-
+@property (weak, nonatomic) IBOutlet UIView *greeView;
+@property (weak, nonatomic) IBOutlet UIView *yellowView;
+@property (weak, nonatomic) IBOutlet UIView *redView;
 @property (nonatomic) UITapGestureRecognizer *tapGestureRecognizer;
+
 @end
 
 @implementation UIApiViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.tapGestureRecognizer = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(handleTap:)];
+    self.tapGestureRecognizer = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tapBaseView:)];
     [self.view addGestureRecognizer:self.tapGestureRecognizer];
+    
+    [self.greeView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapColorView:)]];
+    [self.yellowView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapColorView:)]];
+    [self.redView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapColorView:)]];
 }
 
-- (void)handleTap: (UITapGestureRecognizer *) sender {
+- (void)tapBaseView: (UITapGestureRecognizer *) sender {
     if (sender.state == UIGestureRecognizerStateEnded ){
         CGPoint point = [sender locationInView:self.view];
         NSLog(@"%f, %f", point.x, point.y);
     }
 }
+
+- (void)tapColorView: (UITapGestureRecognizer *) sender {
+    if (sender.state == UIGestureRecognizerStateEnded ){
+
+        CGFloat destY = sender.view.frame.origin.y == 0 ? sender.view.superview.frame.size.height - sender.view.frame.size.height : 0;
+        [UIView animateWithDuration:1.5 animations:^{
+            CGRect rec = sender.view.frame;
+            rec.origin = CGPointMake(sender.view.frame.origin.x, destY);
+            sender.view.frame = rec;
+        }];
+    }
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
