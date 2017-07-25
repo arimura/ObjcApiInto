84478900f200d3f568ff8c3af35c3090522ccbcd
Kotaro Arimura <kotaro_arimura@voyagegroup.com>
2017-07-25 11:38:56 +0900

    NSOperation sample

---
+++ ObjcApiIntroTests/RequestOpTests.m
@@ -0,0 +1,47 @@
+//
+//  RequestOpTests.m
+//  ObjcApiIntro
+//
+//  Copyright © 2017年 hormiga6. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RequestOpTests : XCTestCase
+
+@end
+
+@implementation RequestOpTests
+
+- (void)testSyncRequest {
+    NSBlockOperation *lastOp = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"done lastOp");
+    }];
+    __block NSBlockOperation *googleOp = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"done googleOp");
+    }];
+    __block NSBlockOperation *amazonOp = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"done amazonOp");
+    }];
+    
+    NSOperationQueue *q = [[NSOperationQueue alloc]init];
+    q.maxConcurrentOperationCount = 2;
+    [lastOp addDependency:googleOp];
+    [lastOp addDependency:amazonOp];
+    [q addOperation:lastOp];
+    
+    [[[NSURLSession sharedSession] dataTaskWithURL:[NSURL URLWithString:@"https://google.com"]
+                                completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+         NSLog(@"finish requesting google");
+        [q addOperation:googleOp];
+    }] resume];
+    
+    [[[NSURLSession sharedSession] dataTaskWithURL:[NSURL URLWithString:@"https://amazon.com"]
+                                completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+         NSLog(@"finish requesting amazon");
+        [q addOperation:amazonOp];
+    }] resume];
+    
+    [q waitUntilAllOperationsAreFinished];
+}
+@end
